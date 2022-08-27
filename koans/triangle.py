79e6182bfe6b6270ecf4e748ed16d88b0c4c1f0c
LucileGB <lgarrigoux@hotmail.com>
2022-08-27 11:36:24 +0200

    Contemplated various exceptions (three koans).

--- koans/triangle.py
+++ koans/triangle.py
@@ -17,8 +17,30 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    """
+    First, we check that the triangle is valid (all sides are over 0 and
+    the sum of any two sides is superior to the remaining one).
+    Then, we return an adjective qualifying the triangle's nature.
+    """
+    sides = [a, b, c]
+
+    for side in sides:
+        if side < 1:
+            raise TriangleError
+
+        if (sum(sides) - side) <= side:
+            raise TriangleError
+
+    unique_sides = len(set(sides))
+
+    if unique_sides == 1:
+        return 'equilateral'
+    
+    if unique_sides == 2:
+        return 'isosceles'
+
+    else:
+        return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
